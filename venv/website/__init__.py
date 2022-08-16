c2f848c1d7e763bb8ebe743a7683685c418a9dc6
Magnus Hjornhede <mangeswe@gmail.com>
2022-08-16 20:06:38 +0200

    base.html message block

--- venv/website/__init__.py
+++ venv/website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask  # __ and __ makes a python package?
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)  # __represent name of the file to init flask
     app.config['SECRET_KEY'] = 'fgfgrrtthg'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  # Tell database location
+    db.init_app(app)  # init database
 
     from .views import views
     from .auth import auth
