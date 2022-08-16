06ab563b7f34974174fc1acfbdc620d53bfe78a7
Magnus Hjornhede <mangeswe@gmail.com>
2022-08-16 14:40:08 +0200

    Adding account checks

--- venv/website/auth.py
+++ venv/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)  # Setting up blueprint for flask app
 
@@ -15,4 +15,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # Add user to database
+            pass
+
     return render_template("sign_up.html")
